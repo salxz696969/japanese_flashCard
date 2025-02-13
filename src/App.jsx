de2a6c748cbd477d288ee8fc89641a779e7ec005
visalxz69696969 <saovisal12192005@gmail.com>
2025-02-14 01:38:36 +0700

    kdo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import vocabListFromFile from "./vocab.json";
+import "./styling.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [cardID, setCardID] = useState(0);
@@ -10,23 +12,23 @@ const App = () => {
   const [normalOrRemember, setNormalOrRemember] = useState("normal");
   const [answer, setAnswer] = useState("");
   const [answerColor, setAnswerColor] = useState("");
-  const [lessonVocab, setLessonVocab]= useState("lesson10");
+  const [lessonVocab, setLessonVocab] = useState("lesson1");
   // localStorage.clear();
   useEffect(() => {
     const localVocabList = localStorage.getItem("localVocabList");
     if (localVocabList) {
       const parsedVocab = JSON.parse(localVocabList);
-      setFlashCard(parsedVocab["lesson10"]);
+      setFlashCard(parsedVocab[lessonVocab]);
     } else {
       localStorage.setItem("localVocabList", JSON.stringify(vocabListFromFile));
-      setFlashCard([...vocabListFromFile["lesson10"]]);
+      setFlashCard([...vocabListFromFile[lessonVocab]]);
     }
     const localRemberList = localStorage.getItem("localRemList");
     if (localRemberList) {
       const parsedRem = JSON.parse(localRemberList);
       setRememberList(parsedRem);
     }
-  }, []);
+  }, [lessonVocab]);
 
   useEffect(() => {
     if (vocabListFromFile[lessonVocab]) {
@@ -95,7 +97,6 @@ const App = () => {
         return shuffled;
       });
       setCardID(0);
-      
     }
   }
   function checkAnswer() {
@@ -147,25 +148,53 @@ const App = () => {
     }
   };
   return (
-    <div
-      style={{
-        backgroundColor: "skyblue",
-        padding: "10px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100wh",
-        height: "896px",
-      }}
-    >
+    <div id="container">
+      <select
+        name=""
+        id="select"
+        value={lessonVocab}
+        onChange={(e) => setLessonVocab(e.target.value)}
+      >
+        <option value="lesson1">Lesson 1</option>
+        <option value="lesson2">Lesson 2</option>
+        <option value="lesson3">Lesson 3</option>
+        <option value="lesson4">Lesson 4</option>
+        <option value="lesson5">Lesson 5</option>
+        <option value="lesson6">Lesson 6</option>
+        <option value="lesson7">Lesson 7</option>
+        <option value="lesson8">Lesson 8</option>
+        <option value="lesson9">Lesson 9</option>
+        <option value="lesson10">Lesson 10</option>
+        <option value="lesson11">Lesson 11</option>
+        <option value="lesson12">Lesson 12</option>
+        <option value="lesson13">Lesson 13</option>
+        <option value="lesson14">Lesson 14</option>
+        <option value="lesson15">Lesson 15</option>
+        <option value="lesson16">Lesson 16</option>
+        <option value="lesson17">Lesson 17</option>
+        <option value="lesson18">Lesson 18</option>
+        <option value="lesson19">Lesson 19</option>
+        <option value="lesson20">Lesson 20</option>
+        <option value="lesson21">Lesson 21</option>
+        <option value="lesson22">Lesson 22</option>
+        <option value="lesson23">Lesson 23</option>
+        <option value="lesson24">Lesson 24</option>
+        <option value="lesson25">Lesson 25</option>
+      </select>
+      <div id="show">
+        <button id="show1" onClick={() => setNormalOrRemember("normal")} style={normalOrRemember==="normal"? {backgroundColor:"#C6E7FF"}: {backgroundColor:"#ffffff"}}>
+          Show normal
+        </button>
+        <button id="show2" onClick={() => setNormalOrRemember("remember")} style={normalOrRemember==="remember"? {backgroundColor:"#C6E7FF"}: {backgroundColor:"#ffffff"}}>
+          Show remember
+        </button>
+      </div>
       <button
         style={{
-          height: "200px",
-          width: "374px",
-          fontSize: "2rem",
           backgroundColor: `${answerColor}`,
         }}
         onClick={updateFrontAndBack}
+        id="display"
       >
         {normalOrRemember == "normal"
           ? flashCard.length === 0
@@ -175,56 +204,33 @@ const App = () => {
           ? "ありがとう！何か練習したいことがあれば聞いてね！💪✨"
           : rememberList[remCardID][frontOrBack]}
       </button>
-      <input
-        id="inputBtn"
-        type="text"
-        style={{ width: "365px", margin: "2px", fontSize: "16px" }}
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        onKeyDown={(e) => handleClick(e)}
-      />
-      <br />
-      <select name="" id="" value={lessonVocab} onChange={(e)=>setLessonVocab(e.target.value)}>
-        <option value="lesson10">lesson 10</option>
-        <option value="lesson9">lesson 9</option>
-      </select>
-      <br />
-      <div style={{ marginTop: "10px" }}>
-        <button
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-          onClick={() => backWard()}
-        >
+      <div id="box">
+        <input
+          id="inputBtn"
+          type="text"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+          onKeyDown={(e) => handleClick(e)}
+          placeholder="Type the correct answer..."
+        />
+        <br />
+
+        <br />
+        <button onClick={() => backWard()} className="navR">
           {"<"}
         </button>
-        <button
-          onClick={() => forward()}
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-        >
+        <button onClick={() => forward()} className="navL">
           {">"}
         </button>
         <br />
-        <button
-          onClick={() => moveToRememberList()}
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-        >
+        <button onClick={() => moveToRememberList()} id="rem">
           {normalOrRemember === "normal" ? "Remember" : "Study again"}
         </button>
-        <button
-          onClick={() => changeNormalOrRemember()}
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-        >
-          Show {normalOrRemember === "normal" ? "remember" : "normal"}
-        </button>
-        <button
-          onClick={() => shuffle()}
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-        >
+
+        <button id="con1" onClick={() => shuffle()}>
           Shuffle
         </button>
-        <button
-          style={{ height: "50px", width: "185px", margin: "2px" }}
-          onClick={() => localStorage.clear()}
-        >
+        <button id="con2" onClick={() => localStorage.clear()}>
           Clear memory
         </button>
       </div>
